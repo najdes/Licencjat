41c89ad084ae0f8fbc05d68b47dc5c8dfc31df1f
najdes <krystian.najdecki@gmail.com>
2020-03-28 12:24:17 +0100

    Shooting added

---
+++ LicencjatGame/Assets/Scripts/Gun.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 60f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+    public GameObject blodSplash;
+
+    float nextTimeToFire = 0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+        
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+          Zombie zombie = hit.transform.GetComponent<Zombie>();
+            if(zombie !=null)
+            zombie.TakeDamage(damage);
+
+            if (hit.rigidbody != null)
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+
+            GameObject impactGO = Instantiate(blodSplash, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, .5f);
+
+        }
+        
+    }
+}
---
+++ LicencjatGame/Assets/Scripts/Zombie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
