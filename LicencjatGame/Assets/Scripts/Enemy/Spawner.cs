9b848647c6c307e86a51dd59eac7d5c1fa08e523
najdes <34833954+najdes@users.noreply.github.com>
2020-08-23 18:36:42 +0200

    quick bugfix

--- LicencjatGame/Assets/Scripts/Enemy/Spawner.cs
+++ LicencjatGame/Assets/Scripts/Enemy/Spawner.cs
@@ -7,37 +7,16 @@ public class Spawner : MonoBehaviour
     Animator animator;
     public GameObject zombie;
     public int spawnNumber;
-    public GameObject waypointPrefab;
-    GameObject[] testWpTab = new GameObject[3];
-    Vector3[] waypoints = new Vector3[3];
     float xPos;
     float yPos;
     float zPos;
-    int count=0;
     void Start()
     {
         xPos = gameObject.transform.position.x;
         yPos = gameObject.transform.position.y;
         zPos = gameObject.transform.position.z;
 
-        Debug.Log(gameObject.transform.position);
-
         Instantiate(zombie, new Vector3(xPos, yPos, zPos), Quaternion.identity);
-        
-        Debug.Log("pozycja zombie " + zombie.transform.position);
-        for (int i = 0; i < waypoints.Length; i++)
-        {
-            float npcPosx = gameObject.transform.position.x + Random.Range(-10, 10);
-            float npcPosy = gameObject.transform.position.y;
-            float npcPosz = gameObject.transform.position.z + Random.Range(-10, 10);
-            //zombie.transform.Find(name).position = new Vector3(npcPosx, npcPosy, npcPosz);
-            //Instantiate(waypointPrefab, new Vector3(npcPosx, npcPosy, npcPosz), Quaternion.identity);
-            waypoints[i] = new Vector3(npcPosx,npcPosy,npcPosz);
-            
-
-            // Debug.Log("waypoint "+ i+ " "+ npcPosx + " " + npcPosy + " " + npcPosz);
-            Debug.Log("waypoint " + i + " " + waypoints[i]);
-        }
       
     }
 
