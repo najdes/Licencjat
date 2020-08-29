433942b236fad56844eb3212581c0745eaa12136
najdes <krystian.najdecki@gmail.com>
2020-08-29 18:19:13 +0200

    speed changed

--- LicencjatGame/Assets/Scripts/Player/PlayerMovement.cs
+++ LicencjatGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 8f;
+    public float speed = 4f;
     public float gravity = -20f;
     public float jumpHeight = 2.5f;
     public float WaterHeight = 15.5f;
@@ -40,20 +40,37 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if (x == 0 && z == 0)
+        {
+            animator.SetBool("isWalking", false);
             animator.SetBool("isIdle", true);
+        }
 
         Vector3 move = transform.right * x + transform.forward*z;
         controller.Move(move * speed * Time.deltaTime);
 
         if(Input.GetButtonDown("Jump")&& isGrounded)
         {
+            animator.Play("BasicMotions@Jump01");
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
 
+        if (Input.GetKey("left shift") && isGrounded)
+        {
+            speed = 8f;
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+            speed = 4f;
+        }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
         animator.SetBool("isIdle", false);
-        animator.SetBool("isWalking", true);
+        animator.SetBool("isJumping", false);
+        
     }
 
    
